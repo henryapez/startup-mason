50f992aeabad8e736f2011e05a7225c442dd0bb6
fellert <ethan.ellert@gmail.com>
2016-08-13 18:28:15 -0400

    Initial commit

---
+++ js/events-data.js
@@ -0,0 +1,4 @@
+var canvasEvents = {
+	'08-29-2016' : '<a href="http://google.com" target=_blank>First day of classes</a>',
+	'12-14-2015' : '<a href="http://google.com" target=_blank>Amet Temporibus ad quod enim dolor doloribus sequi!</a>'
+};
