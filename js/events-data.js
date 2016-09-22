9050a4ae7646fa627da02ac3d4da977caffc6415
fellert <ethan.ellert@gmail.com>
2016-09-22 09:43:04 -0400

    updates

--- js/events-data.js
+++ js/events-data.js
@@ -1,4 +1,10 @@
 var canvasEvents = {
+	'11-16-2016' : '<a href="http://google.com" target=_blank>How to get a job at and internship</a>',
+	'11-02-2016' : '<a href="http://google.com" target=_blank>Dark Side of Entrepreneurship</a>',
+	'10-19-2016' : '<a href="http://google.com" target=_blank>Funding Entrepreneurship</a>',
+	'10-05-2016' : '<a href="http://google.com" target=_blank>Food Entrepreneurship</a>',
+	'09-21-2016' : '<a href="http://google.com" target=_blank>Social Entrepreneurship</a>',
+	'09-07-2016' : '<a href="http://google.com" target=_blank>Fall Kickoff</a>',
 	'08-29-2016' : '<a href="http://google.com" target=_blank>First day of classes</a>',
 	'12-14-2015' : '<a href="http://google.com" target=_blank>Amet Temporibus ad quod enim dolor doloribus sequi!</a>'
 };
